cc22df4886f1a9fe516a64f13ba6f877354311ca
Drkoukichi <78055680+Drkoukichi@users.noreply.github.com>
2021-11-09 21:18:40 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-let receivedate = [0,0,0]
+let receivedate = "000"
+let protocol = 0
 let votedate = 0
+let reserve = 0
 let votecount = [0]
 let ansno = 2
+
+function changeint (i : string){
+    let t : string
+    t = receivedate.substr(0,1)
+    protocol = parseFloat(t)
+    t = receivedate.substr(1,1)
+    votedate = parseFloat(t)
+    t = receivedate.substr(2,1)
+    reserve = parseFloat(t)
+}
+
+function sendradio()
 
 function setans(){
     
